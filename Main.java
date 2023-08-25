581da21fa28b4f261e54b834d609009424267f5d
Gaurav Sinha <gauravsinha582@gmail.com>
2023-08-25 13:30:30 -0400

    Added features

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        String x = "Hello, Gaurav!";
+        String x = "Hello, Gaurav!!!";
         System.out.println(x);
+        int a = 3, b= 6, c, d;
+        c = a + b;
+        System.out.println(c);  //Sum
+        d = a * b;              //Product
+        System.out.println(d);
     }
 }
